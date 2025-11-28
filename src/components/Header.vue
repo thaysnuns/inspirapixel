7c4ca263be59f675a342ac7e975ce65ebefa6372
thaysnuns <nunesthayna2000@gmail.com>
2025-11-28 13:18:19 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+import logo from "./assets/logo.png"
+
+</script>
+
+<template>
+<header>
+    <img :src="logo" alt="Imagem da logo marca">
+   <nav>
+    <li>
+        <a href="">
+            <Icon class="search-icon" icon="material-symbols:search" />
+            <!-- <Icon icon="coleção:nomeDoIconeEmIngles" -->
+        </a>
+    </li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+   </nav>
+</header>
+</template>
+
+<style scoped lang="scss">
+
+</style>
