978a525a4ed07f93a82ecb51f19e38408c8237d2
thaysnuns <nunesthayna2000@gmail.com>
2025-12-03 12:17:43 -0300

    feat: add image cards

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps(["imagem"]);
+//o que é o defineProps
+//é uma função do vue que permite receber dados em comunicação de componentes
+</script>
+
+<template>
+
+    <div class="card">
+        <img :src="imagem" alt="Imagem do card">
+    </div>
+    
+</template>
+
+<style scoped lang="scss">
+.card{
+    //border: solid;
+    width: 35vw;
+   
+    img{
+        width: 100%;
+        height: 100%;
+        border-radius: 10px;
+    }
+
+    
+}
+</style>
