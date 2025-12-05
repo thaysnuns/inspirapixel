04acc0b5946059f3915ad7ef5240ff8b598e9f1c
thaysnuns <nunesthayna2000@gmail.com>
2025-12-05 14:00:21 -0300

    style: add new favorite heart on the card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,27 +2,40 @@
 defineProps(["imagem"]);
 //o que é o defineProps
 //é uma função do vue que permite receber dados em comunicação de componentes
+import favoritos from "../assets/favoritos.png";
 </script>
 
 <template>
-
-    <div class="card">
-        <img :src="imagem" alt="Imagem do card">
-    </div>
-    
+  <div class="card">
+    <span class="fav"><img :src="favoritos" alt="" /></span>
+    <img :src="imagem" alt="Imagem do card" />
+  </div>
 </template>
 
 <style scoped lang="scss">
-.card{
-    //border: solid;
-    width: 35vw;
-   
-    img{
-        width: 100%;
-        height: 100%;
-        border-radius: 10px;
-    }
+.card {
+  //border: solid;
+  position: relative;
+  width: 15vw;
+  height: 50vh;
 
-    
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+  }
+
+  .fav {
+    position: absolute;
+    top: 8px;
+    right: 8px;
+    width: 15px;
+    height: 15px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
