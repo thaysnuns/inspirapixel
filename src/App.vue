a121a62a8bab824577e626463e3559b88421055c
thaysnuns <nunesthayna2000@gmail.com>
2025-12-04 22:22:57 -0300

    fix: fix import

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
+// lógica
+import Banner from './components/Banner.vue';
 import Header from './components/Header.vue';
+import Inspire from './components/Inspire.vue';
 </script>
 
 <template>
+    <!-- as tags HTML -->
 <Header/>
+<Banner/>
+<Inspire/>
 </template>
 
 <style></style>
