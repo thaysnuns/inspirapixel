5e0b6c9d51d70674fc78b64cfd1e4a6c9178e4b8
thaysnuns <nunesthayna2000@gmail.com>
2025-12-05 14:00:51 -0300

    style: adjust size images

--- src/components/Inspire.vue
+++ src/components/Inspire.vue
@@ -1,30 +1,48 @@
 <script setup>
 import Card from "./Card.vue";
+//importamos o axios -> biblioteca para fazer requisições HTTP
+import axios from "axios";
+
+//importamos o ref -> uma ferramenta que permite criar variaveis reativas
+import { ref } from "vue";
+
+//criar uma variave reativa que vai receber a lista de imagens da API
+const imagens = ref([]);
+
+//função assincrona -> pra buscar a API
+async function carregarImagens() {
+  //res -> resposta da API
+  //axios pega os dados get axios.get()
+  //await vc precisa esperar o axios ir buscar as imagens
+  const res = await axios.get("https://picsum.photos/v2/list?page=5&limit=30");
+
+  imagens.value = res.data;
+  //guardando as imagens da API dentro da variavel imagens
+  //
+  console.log(res);
+}
+
+carregarImagens();
 </script>
 
 <template>
   <h2>Inspire-se</h2>
   <section class="inspire">
-    <Card
-      imagem="https://i.pinimg.com/736x/cc/0d/37/cc0d3781240d0fbfe57194dd56898c88.jpg"
-    />
-    <Card
-      imagem="https://i.pinimg.com/236x/2a/bd/f1/2abdf10d67683fc2354f5aeac3b9a69c.jpg"
-    />
-    <Card
-      imagem="https://www.publicdomainpictures.net/pictures/280000/nahled/night-landscape-1546338197B2k.jpg"
-    />
-    <Card
-      imagem="https://www.publicdomainpictures.net/pictures/280000/nahled/night-landscape-1546338197B2k.jpg"
-    />
+    <Card v-for="img in imagens" :imagem="img.download_url" />
   </section>
 </template>
 
 <style scoped lang="scss">
-.inspire, h2{
-    display: flex;
-    justify-content: space-between;
-    gap: 10px;
-    margin: 2rem;
+h2 {
+  //justify-content: space-evenly;
+  margin: 2rem;
+}
+
+.inspire {
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 2rem;
 }
 </style>
